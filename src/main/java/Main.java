543e20cf47ea3304054ee83c5a4ca9a3505f3355
Polina Shebysheva <gatsbysober@gmail.com>
2023-01-08 23:51:17 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 99 ;
+        for (int i = 10; i <= x; i++) {
+            if (i * i >=x) {
+                System.out.println();
+            }
+        }
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.sqr;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+    @Test
+    public void calcTestWhenBothNumberFinded() {
+        SQRService service = new SQRService();
+        int actual = service.calc(200, 300);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void calcTestWhenOneNumberFinded () {
+        SQRService service = new SQRService();
+        int actual = service.calc(0, 100);
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void calcTestWhenZeroNumberFinded () {
+        SQRService service = new SQRService();
+        int actual = service.calc(50, 99);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
